ea4eb648b2986bf9119f094a5d89c5fd37d94892
JMEN98 <78163678+JMEN98@users.noreply.github.com>
2022-12-08 21:26:44 -0500

    Initial commit

---
+++ src/pongv2/Paddle.java
@@ -0,0 +1,62 @@
+package pongv2;
+import java.awt.*;
+import  java.awt.event.*;
+
+public class Paddle extends  Rectangle {
+    int id;
+    int yVelocity;
+    int speed =10;
+    Paddle(int x,int y,int PADDLE_WIDTH,int PADDLE_HEIGHT,int id){
+        super(x,y,PADDLE_WIDTH,PADDLE_WIDTH);
+        this.id=id;
+
+    }
+    public  void keyPressed(KeyEvent e){
+        switch (id){
+            case 1:
+                if(e.getKeyCode()==KeyEvent.VK_W){
+                    setDirection(-speed);
+                    move();;
+                }
+                if(e.getKeyCode()==KeyEvent.VK_S){
+                    setDirection(speed);
+                    move();;
+                }
+                break;
+            case 2:
+                if(e.getKeyCode()==KeyEvent.VK_UP){
+                    setDirection(-speed);
+                    move();;
+                }
+                if(e.getKeyCode()==KeyEvent.VK_DOWN){
+                    setDirection(speed);
+                    move();;
+                }
+                break;
+        }
+
+    }
+
+    public  void keyReleased(KeyEvent e){
+
+    }
+
+    public void setDirection(int yDirection){
+        yVelocity=yDirection;
+
+    }
+
+    public void  move(){
+        y=y+yVelocity;
+
+    }
+
+    public  void  draw( Graphics g){
+        if(id==1)
+            g.setColor(Color.MAGENTA);
+
+        else g.setColor(Color.green);
+        g.fillRect(x,y,width,height);
+
+    }
+}
